b641762bbe04609a2a4c3ecc00f41ca955ae97b1
Michele Rignanese <mrignanese74@gmail.com>
2015-06-01 13:24:04 +0000

    refined search parameters options & compounds

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,13 @@
 class SearchesController < ApplicationController
  
   def index
-   @q = Theme.all.ransack(params[:q])
+    @q = Theme.all.ransack(params[:q].try(:merge, m: 'or'))
+    
+    ##### Grouping queries by OR instead of AND #####
+    #@q = Theme.all.ransack(params[:q].try(:merge, m: 'or'))
+    #  Normally, if you wanted users to be able to toggle between AND and OR query grouping, you would probably set up your search form so that m was in the URL params hash  
+
+    
    @search = @q.result(distinct: true)
    @q.build_condition if @q.conditions.empty?
    @q.build_sort if @q.sorts.empty?    
