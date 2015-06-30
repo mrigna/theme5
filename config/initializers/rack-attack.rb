c345bfdd810db21cdc9f9709ec467376218fff11
Michele Rignanese <mrignanese74@gmail.com>
2015-06-30 13:23:29 +0000

    heroku specific

---
+++ config/initializers/rack-attack.rb
@@ -0,0 +1,7 @@
+# In config/initializers/rack-attack.rb
+class Rack::Attack
+   Rack::Attack.whitelist('allow from fps') do |req|
+    # Requests are allowed if the return value is truthy
+    '193.191.211.19' == req.ip
+  end
+end
