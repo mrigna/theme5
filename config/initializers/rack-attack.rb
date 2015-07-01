7db14e2b2af4244f6d989df86f138df2e99ec72a
Michele Rignanese <mrignanese74@gmail.com>
2015-07-01 09:45:08 +0000

    adapted rackup for rack-attack12

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,8 +1,4 @@
-class Rack::Attack::Request < ::Rack::Request
-      def fps?
-        '193.191.211.19' == env['HTTP_X_FORWARDED_FOR']
-      end
+Rack::Attack.whitelist('allow from fps') do
+    # Requests are allowed if the return value is truthy
+  '193.191.211.19' == env['HTTP_X_FORWARDED_FOR']
 end
-
-  
-Rack::Attack.whitelist("allow from fps") {|req| req.fps? }
