1464be697ee65b34229dcb08d742797a47bf2150
Michele Rignanese <mrignanese74@gmail.com>
2015-07-01 05:49:06 +0000

    adapted rackup for rack-attack

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Theme5
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.middleware.use Rack::Attack
   end
 end
