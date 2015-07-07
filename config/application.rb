2e051b0f7c9c35e6de29acbffbf24ce23f2d47af
Michele Rignanese <mrignanese74@gmail.com>
2015-07-07 10:57:46 +0000

    clear environment

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-IP_WHITELIST = %w(193.191.211.19)
+#IP_WHITELIST = %w(193.191.211.19)
 
 module Theme5
   class Application < Rails::Application
-    config.middleware.use Rack::IpFilter, IpFilter::WhiteList.new(::IP_WHITELIST)   
+    #config.middleware.use Rack::IpFilter, IpFilter::WhiteList.new(::IP_WHITELIST)   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- db/migrate/20150626141619_add_missing_fields_to_node.rb
+++
@@ -1,5 +0,0 @@
-class AddMissingFieldsToNode < ActiveRecord::Migration
-  def change
-    add_column :nodes, :group, :string 
-  end
-end
