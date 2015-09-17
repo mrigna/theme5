e13f44d27393264af631312495294dbee9524717
Michele Rignanese <mrignanese74@gmail.com>
2015-09-17 19:27:29 +0000

    adapted documents migration db

--- db/migrate/20150912101631_add_terms_to_metadata.rb
+++
@@ -1,7 +0,0 @@
-class AddTermsToMetadata < ActiveRecord::Migration
-  def change
-    add_column :metadata, :term1_id, :integer
-    add_column :metadata, :term2_id, :integer
-    add_column :metadata, :term3_id, :integer
-  end
-end
